c75df0299c24443c36b0e6fbdfd87b576ffd7c82
dperel <davidperel.pdf@gmail.com>
2015-09-01 13:28:09 -0400

    Set the routes constraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,11 @@
-Rails.application.routes.draw do
+MarketPlaceApi::Application.routes.draw do
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/'  do
+    # We are going to list our resources here
+  end
 end
+
+
+#purpose of setting path above: 
+#"This is telling Rails to set the starting path for each request to be root in relation to the subdomain"
